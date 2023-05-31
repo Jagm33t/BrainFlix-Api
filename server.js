8a14c5d4789ca0110cc5c04dd66365afc7e73c5a
Jagm33t <130860323+Jagm33t@users.noreply.github.com>
2023-05-30 22:39:13 -0700

    Backend Api calls are setup

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const Videos = require('./routes/Videos');
+//Images served as static assets
+app.use(express.static('./public'));
+
+app.use(cors());
+
+const port = 8080;
+
+app.use('/videos', Videos);
+
+app.listen(port, () => {
+  console.log(`Express demo listening at port ${port}`);
+});
